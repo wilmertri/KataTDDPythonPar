a507da4ee9815ae52fc54d25a5b05d6795af2975
Wilmer Triana <fabiantriana1072@gmail.com>
2017-10-01 14:37:52 -0500

    Iteración 1 - Ciclo 1 - Se realiza el test para que falle con el string vacio (Rojo)

---
+++ Estadisticas.py
@@ -0,0 +1,4 @@
+
+class Estadisticas():
+    def calcular(self, cadena):
+        pass
---
+++ EstadisticasTest.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+from Estadisticas import Estadisticas
+
+class EstadisticasTest(TestCase):
+    def test_calcular(self):
+        self.assertEqual(Estadisticas().calcular(""),[],"Cadena vacia")
