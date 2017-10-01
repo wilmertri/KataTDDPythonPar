b2e5050db3280a8c4eca3b41fd2bed1ee6b9572a
Wilmer Triana <fabiantriana1072@gmail.com>
2017-10-01 15:23:57 -0500

    Iteración 1 - Ciclo 4 - Se realiza el test para que falle enviando multiples numeros (Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -11,3 +11,6 @@ class EstadisticasTest(TestCase):
 
     def test_calcular_conDosNumeros(self):
         self.assertEqual(Estadisticas().calcular("1,3"),[2],"Dos numeros")
+
+    def test_calcular_conMultiplesNumeros(self):
+        self.assertEqual(Estadisticas().calcular("1,3,4,8"),[4],"Multiples numeros")
